78e7fa068b834cf9a52fea68dfbcf4b4c7576e4f
Max Shkurygin <max.atreides@gmail.com>
2014-08-25 18:58:39 -0400

    removed flatpages, added a index view message, fixed requirements.txt

--- spacedonkey/spacedonkey/urls.py
+++ spacedonkey/spacedonkey/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'spacedonkey.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'spacedonkey.views.index', name='index'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^pages/', include('django.contrib.flatpages.urls')),
 )
