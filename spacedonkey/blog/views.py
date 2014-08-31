d2bbeb3f6f3ade14a463b797adcff8b9149a4a78
Max Shkurygin <max.atreides@gmail.com>
2014-08-30 23:10:03 -0400

    added a basic list_view to the blog

--- spacedonkey/blog/views.py
+++ spacedonkey/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
+def list_view(request):
+    post_list = Post.objects.all()
+    context = {'post_list': post_list}
+    return render(request, 'blog/list_view.html', context)
--- spacedonkey/spacedonkey/urls.py
+++ spacedonkey/spacedonkey/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'spacedonkey.views.index', name='index'),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'^admin/', include(admin.site.urls)),
 )
