3242802cbffa4479b709d5a350eca6e65854c007
Max Shkurygin <max.atreides@gmail.com>
2014-09-11 22:54:02 -0400

    Added new fields to Post and added a Tag model

--- spacedonkey/blog/models.py
+++ spacedonkey/blog/models.py
@@ -1,13 +1,39 @@
+from markdown import markdown
 from django.db import models
+from django.utils.text import slugify
 
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=15, unique=True, primary_key=True)
+    description = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200, editable=False, default='')
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Tag, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     """ Basic blog post """
 
-    title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    text = models.TextField()
+    title = models.CharField(max_length=200, unique=True, primary_key=True)
+    slug = models.CharField(max_length=200, editable=False, default='')
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    text = models.TextField(blank=False)
+    text_markdown = models.TextField(editable=False)
+    tags = models.ManyToManyField(Tag, blank=True, null=True)
+
+
+    def save(self, *args, **kwargs):
+        self.text_markdown = markdown(self.text)
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['-pub_date']
