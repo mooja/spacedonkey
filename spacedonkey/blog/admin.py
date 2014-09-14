8ae900a4786fd47b7b1fc94b886bc14bae52cf1b
Max Shkurygin <max.atreides@gmail.com>
2014-09-14 01:28:57 -0400

    Beautified admin posts view

--- spacedonkey/blog/admin.py
+++ spacedonkey/blog/admin.py
@@ -3,18 +3,26 @@ from blog.models import Post, Tag
 
 
 class PostAdmin(admin.ModelAdmin):
-    readonly_fields = ('text_markdown', 'slug')
-    search_fields = ['title', 'text']
-    view_on_site = True
-    date_hierarchy = 'pub_date'
-    fields = (('pub_date', 'title', 'slug'), 'text', 'tags', 'text_markdown')
+    fields = (('pub_date', 'title', 'slug'), ('text', 'text_html_with_tags'), 'tags')
+    readonly_fields = ('text_html_with_tags',)
+    search_fields = ['title', 'tags__name']
+    list_filter = ('pub_date',)
     list_display = ('title', 'pub_date')
+    ordering = ('-pub_date',)
+    prepopulated_fields = {'slug': ('title',)}
+
+    def text_html_with_tags(self, instance):
+        return instance.text_html
+    text_html_with_tags.allow_tags = True
+    text_html_with_tags.short_description = ""
+
+    save_on_top = True
 
 
 class TagAdmin(admin.ModelAdmin):
     readonly_fields = ['slug', 'num_posts']
     fields = (('name', 'slug', 'num_posts'),)
-    list_display = ['name', 'num_posts', 'slug']
+    list_display = ['name', 'num_posts']
 
 
 admin.site.register(Post, PostAdmin)
