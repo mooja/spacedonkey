85b68399a55181798b6a32fece49f5edb39e7040
Max Shkurygin <max.atreides@gmail.com>
2014-09-16 00:47:37 -0400

    Display up to ten most popular tags.

--- spacedonkey/spacedonkey/views.py
+++ spacedonkey/spacedonkey/views.py
@@ -4,7 +4,7 @@ from blog.models import Post, Tag
 
 def index(request):
         last_three_posts = Post.objects.all().order_by('-pub_date')[:3]
-        popular_tags = Tag.objects.all()[:6]
+        popular_tags = Tag.objects.all()[:10]
         c = {'last_three_posts': last_three_posts,
              'popular_tags': popular_tags}
         return render(request, "index.html", c)
