9e54ef1b45976c2f57c5a9c8e1eea25e65557309
Max Shkurygin <max.atreides@gmail.com>
2014-09-16 00:04:45 -0400

    Index view now displays: three latest blog entries (if they exist), three random tags.

--- spacedonkey/spacedonkey/views.py
+++ spacedonkey/spacedonkey/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from blog.models import Post, Tag
+
 
 def index(request):
-    return render(request, "index.html")
+        last_three_posts = Post.objects.all().order_by('-pub_date')[:3]
+        popular_tags = Tag.objects.all()[:6]
+        c = {'last_three_posts': last_three_posts,
+             'popular_tags': popular_tags}
+        return render(request, "index.html", c)
