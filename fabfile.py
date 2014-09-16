9d682820099c03d28308597b740a0ebaa32535f2
Max Shkurygin <max.atreides@gmail.com>
2014-09-16 01:51:28 -0400

    Updated deployment script

---
+++ fabfile.py
@@ -0,0 +1,34 @@
+from fabric.api import *
+
+
+env.hosts.append('mooja@spacedonkey.org')
+env.user = 'mooja'
+local_root = '/home/mooja/spacedonkey_project/spacedonkey'
+remote_root = '/home/mooja/spacedonkey'
+
+def commit():
+    with settings(warn_only=True):
+        local("git commit -p")
+
+
+def push():
+    local("git push")
+
+
+def upstream_restart_webserver():
+    sudo("restart spacedonkey")
+
+
+def upstream_pull():
+    with cd("/home/mooja/spacedonkey"):
+        run("git pull")
+
+
+def deploy():
+    lcd(local_root)
+    commit()
+    push()
+    with cd(remote_root):
+        upstream_pull()
+        upstream_restart_webserver()
+    print("Deployment complete.")
