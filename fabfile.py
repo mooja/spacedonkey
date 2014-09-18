39f1aee0d8ce0c3e9be22623069d1231b05abc19
Max Shkurygin <max.atreides@gmail.com>
2014-09-17 23:11:14 -0400

    Improved deployment script. Adjust fixtures script.

--- fabfile.py
+++ fabfile.py
@@ -1,38 +1,54 @@
 from fabric.api import *
 
 
-env.hosts.append('mooja@spacedonkey.org')
+upstream_host = 'mooja@spacedonkey.org'
+
+project_root = '/home/mooja/spacedonkey/'
+app_root = '/home/mooja/spacedonkey/spacedonkey/'
+python_path = "/home/mooja/envs/spacedonkey/bin/python"
+
 env.user = 'mooja'
-local_root = '/home/mooja/spacedonkey_project/spacedonkey'
-remote_root = '/home/mooja/spacedonkey'
+env.hosts.append(upstream_host)
+
 
 def commit():
-    with settings(warn_only=True):
-        local("git commit -p")
+    with cd(project_root):
+        run("git commit")
 
 
 def push():
-    local("git push")
+    with cd(project_root):
+        run("git push")
 
 
-def upstream_restart_webserver():
-    sudo("restart spacedonkey")
-
-
-def upstream_pull():
-    with cd("/home/mooja/spacedonkey"):
+def pull():
+    with cd(project_root):
         run("git pull")
 
 
+def restart_webserver():
+    sudo("restart spacedonkey")
+
+
 def deploy():
-    # commit localy
-    with lcd(local_root):
+    print("Preparing to deploy...")
+
+    # commit locally
+    with settings(host='localhost'):
         commit()
         push()
 
-    # pull and restart web server remotely
-    with cd(remote_root):
-        upstream_pull()
-        upstream_restart_webserver()
+    # pull remotely and restart server
+    pull()
+    restart_webserver()
 
     print("Deployment complete.")
+
+
+def load_fixtures():
+    with cd(app_root):
+        with settings(warn_only=True):
+            if not run("test -f db.sqlite3"):
+                run("rm db.sqlite3")
+        run("{python_path} manage.py migrate".format(python_path=python_path))
+        run("{python_path} fixtures/load.py".format(python_path=python_path))
---
+++ spacedonkey/fixtures/load.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import random
+import string
+
+sys.path.append(os.getcwd())
+os.environ['DJANGO_SETTINGS_MODULE'] = 'spacedonkey.settings'
+import django
+django.setup()
+
+# non system imports
+import faker
+import pytz
+
+
+from blog.models import Post, Tag
+
+
+def generate_posts(tags, n=10, tags_per_post=5):
+    fake = faker.Faker()
+
+    eastern = pytz.timezone('US/Eastern')
+
+    dir_name = os.path.dirname(__file__)
+    lsp_quotes = open(os.path.join(dir_name, "lsp_quotes.txt")).readlines()
+    lsp_quotes = map(string.strip, lsp_quotes)
+    lsp_quotes = map(unicode, lsp_quotes)
+
+    # get a maximum possible sample of lsp quotes (<= 65)
+    max_quotes = min(n, len(lsp_quotes))
+    quotes = random.sample(lsp_quotes, max_quotes)
+
+    # generate a random sentence if not enough lsp quotes
+    if n > max_quotes:
+        for i in range(n - max_quotes):
+            quotes.append(fake.sentence())
+
+    # create posts
+    posts = []
+    for i in range(n):
+        p = Post()
+        p.title = quotes[i]
+        p.pub_date = eastern.localize(fake.date_time_this_decade())
+        p.text = "\n\n".join(fake.paragraphs(5))
+        p.save()
+
+        p.tags = random.sample(tags, tags_per_post)
+        p.save()
+
+        posts.append(p)
+
+    return posts
+
+
+def generate_tags(n=10):
+    fake = faker.Faker()
+
+    # create tags
+    tags = []
+    for i in range(n):
+        t = Tag()
+        t.name = fake.word()
+        t.description = fake.sentence()
+        t.save()
+        tags.append(t)
+
+    return tags
+
+
+def dump_tables():
+    # remove previous objects
+    # technically we're not dumping these
+    pass
+
+
+def load():
+    # must generate tags before posts
+    tags = generate_tags(20)
+    posts = generate_posts(tags, 20)
+    return posts
+
+
+def run():
+    print("Adding tags and posts...")
+    if load():
+        print("Fixtures have been generated and added to the tables. Mathematical!")
+
+if __name__ == '__main__':
+    run()
