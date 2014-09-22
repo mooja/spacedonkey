c74acfe624cdf1d921d005ed08598f95eeb579aa
Max Shkurygin <max.atreides@gmail.com>
2014-09-22 13:36:58 -0400

    Added optional tag filter for post list views

--- spacedonkey/blog/urls.py
+++ spacedonkey/blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
     url(r'^date/(?P<year>\d{4})/$', PostList.as_view(), name='post-list'),
     url(r'^date/(?P<year>\d{4})/(?P<month>\d{1,2})/$', PostList.as_view(), name='post-list'),
     url(r'^date/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})$', PostList.as_view(), name='post-list'),
+    url(r'^tag/(?P<tag>[-_\w]+)/$', PostList.as_view(), name='post-list'),
 )
--- spacedonkey/blog/views.py
+++ spacedonkey/blog/views.py
@@ -3,6 +3,18 @@ from blog.models import Post, Tag
 from django.views.generic import DetailView, ListView
 
 
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        context['all_tags_list'] = Tag.objects.all()
+        context['recent_posts'] = Post.objects.all()[:5]
+        context['all_post_list'] = Post.objects.order_by('-pub_date')
+        return context
+
+
 class PostList(ListView):
     model = Post
     context_object_name = 'post_list'
@@ -12,11 +24,16 @@ class PostList(ListView):
         context['year'] = self.kwargs.get('year', None)
         context['month'] = self.kwargs.get('month', None)
         context['day'] = self.kwargs.get('day', None)
+        context['tag'] = self.kwargs.get('tag', None)
 
         return context
 
     def get_queryset(self):
         query = Post.objects.all()
+
+        if self.kwargs.get('tag', None):
+            tag = self.kwargs['tag']
+            query = query.filter(tags__slug=tag)
 
         if self.kwargs.get('year', None):
             year = int(self.kwargs['year'])
@@ -31,16 +48,3 @@ class PostList(ListView):
             query = query.filter(pub_date__day=day)
 
         return query
-
-
-
-class PostDetailView(DetailView):
-    model = Post
-    context_object_name = 'post'
-
-    def get_context_data(self, **kwargs):
-        context = super(PostDetailView, self).get_context_data(**kwargs)
-        context['all_tags_list'] = Tag.objects.all()
-        context['recent_posts'] = Post.objects.all()[:5]
-        context['all_post_list'] = Post.objects.order_by('-pub_date')
-        return context
